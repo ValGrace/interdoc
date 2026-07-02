c99de9abd343969c66bddd491f2c759850b3b73f
ValGrace <gracevalerie1@gmail.com>
2026-07-02 17:04:36 +0300

    refactor: swith model

--- app/vectorstore.py
+++ app/vectorstore.py
@@ -4,7 +4,7 @@ from chromadb.utils import embedding_functions
 # persistent storage (chromadb is a vector database)
 client = chromadb.PersistentClient(path="./data/chroma_db")
 
-embedding_fn = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-l6-v2")
+embedding_fn = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
 
 collection = client.get_or_create_collection(name="documents", embedding_function=embedding_fn)
 
